b406ff0b1f1902eb1b641d0c112e2a591eeae8d5
이재원 <jenyglee_30@naver.com>
2022-11-02 11:08:02 +0900

    디렉토리 구조 변경, 방명록 화면 작업

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+from pymongo import MongoClient
+
+client = MongoClient('민선님 몽고디비 주소')
+db = client.dbsparta
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/guest-book')
+def guestBook():
+    return render_template('guest-book.html')
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
